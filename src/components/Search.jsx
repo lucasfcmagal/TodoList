9a1dad1d6a0dc20802b35f00706403bce14b14e7
lucasfcmagal <166072474+lucasfcmagal@users.noreply.github.com>
2024-06-10 17:44:21 -0300

    updateProjetc

---
+++ src/components/Search.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable react/prop-types */
+const Search = ({ search, setSearch }) => {
+  return (
+    <div className="search">
+      <h2>Pesquisar:</h2>
+      <input
+        type="text"
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+        placeholder="Digite para pesquisar..."
+      />
+    </div>
+  );
+};
+
+export default Search;
